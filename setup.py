f15ddb7a58281f4dcbc7bda54764554c3d3fc913
Nathan <nathan@nathan-s.com>
2022-03-03 02:57:37 -0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(name='ESIRabbit',
+                 version='0.1',
+                 description='ESI task queues for distributed API access using RabbitMQ and Redis.',
+                 author_email='maintainers@eveinsight.net',
+                 url='https://github.com/EVEInsight/ESIRabbit',
+                 packages=['ESIRabbit'],
+                 install_requires=[
+                       'Celery>=5.2,<6',
+                       'python-dateutil>=2.8.2,<3',
+                       'redis>=4.1,<5',
+                       'requests>=2.27,<3'
+                 ],
+                 python_requires=">=3.10"
+                 )
