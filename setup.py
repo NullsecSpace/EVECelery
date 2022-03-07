909b82b093f454b49641b67f33e319a3fa7e4413
Nathan <nathan@nathan-s.com>
2022-03-07 14:55:37 -0800

    Adjust setup.

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
 import setuptools
-import ESICelery
 
+pkg_info = {}
+with open("ESICelery/__version__.py", "r", encoding="utf-8") as f:
+    for l in f.readlines():
+        data = l.split('=')
+        pkg_info[data[0].strip()] = (data[1].replace("'", "")).strip()
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setuptools.setup(name='ESICelery',
-                 version=ESICelery.__version__,
-                 description=ESICelery.__description__,
-                 license=ESICelery.__license__,
+                 version=pkg_info['__version__'],
+                 description=pkg_info['__description__'],
+                 license=pkg_info['__license__'],
                  long_description=long_description,
                  long_description_content_type="text/markdown",
-                 author_email=ESICelery.__author_email__,
-                 url=ESICelery.__url__,
+                 author_email=pkg_info['__author_email__'],
+                 url=pkg_info['__url__'],
                  packages=setuptools.find_packages(include=["ESICelery", "ESICelery.*"]),
                  install_requires=[
                        'Celery>=5.2,<6',
