6426195471bc0850c5dc6e1ff1217bb873780d23
Nathan <nathan@nathan-s.com>
2023-03-13 20:05:22 -0700

    Autoapi.

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Nathan'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['autoapi.extension']
+
+autoapi_dirs = ['../EVECelery']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
