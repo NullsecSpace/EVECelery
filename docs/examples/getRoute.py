d99f523af22974e58581190cb4fe52709aad9546
Nathan <nathan@nathan-s.com>
2023-03-13 19:53:47 -0700

    Docs quickstart.

--- docs/examples/getRoute.py
+++
@@ -1,6 +0,0 @@
-from EVECelery.CeleryApps import CeleryWorker
-from EVECelery.tasks.Routes import *
-
-CeleryWorker.create_class()
-r = Route().get_sync(timeout=5, origin=30000142, destination=30002659)  # Jita to Dodixie - 12 jumps
-print(f"Jumps: {len(r) - 1} Systems: {r} ")
--- docs/examples/getSystem.py
+++
@@ -1,6 +0,0 @@
-from EVECelery.CeleryApps import CeleryWorker
-from EVECelery.tasks.Universe import *
-
-CeleryWorker.create_class()
-r = SystemInfo().get_sync(timeout=5, system_id=30000142)
-print(r)
